25fffb0fc9f74d9d7142405c2e3100763b4c0591
Im Zack <54569690+jakaprayudha@users.noreply.github.com>
2024-11-20 13:25:09 +0700

    Delete database directory

--- database/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-$host = "localhost";
-$uname = "root";
-$password = "";
-$database = "db_mbkm";
-$koneksi = mysqli_connect($host, $uname, $password, $database);
